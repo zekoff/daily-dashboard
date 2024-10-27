c089104693f9bd1c02bf6c500f1b8cfc3b2033c8
Michael Zekoff <zekoff@gmail.com>
2024-10-27 14:24:04 -0500

    Add headlines card

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,8 +11,9 @@ import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import axios from "axios";
 
-const NASA_API_KEY = "5YNzXNqZ25GQ4TL62ASPnaVisVtBGIipV24NxX5B";
+const NASA_API_KEY = process.env.NASA_API_KEY;
 const NASA_APOD_URL = "https://api.nasa.gov/planetary/apod";
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 export const getNasaApod = onRequest({cors: true}, async (req, res) => {
   try {
@@ -37,5 +38,24 @@ export const getQotd = onRequest({cors: true}, async (req, res) => {
   } catch (error) {
     logger.error("Error fetching QOTD:", error);
     res.status(500).send("Error fetching QOTD");
+  }
+});
+
+export const getHeadlines = onRequest({cors: true}, async (req, res) => {
+  try {
+    const callResult = await axios.get("https://api.thenewsapi.com/v1/news/top", {
+      params: {
+        locale: "us",
+        api_token: NEWS_API_KEY,
+        categories: "general,science,tech",
+        language: "en",
+        limit: 10,
+      },
+    });
+    logger.info(callResult.data);
+    res.json({data: callResult.data});
+  } catch (error) {
+    logger.error("Error fetching headlines:", error);
+    res.status(500).send("Error fetching headlines");
   }
 });
